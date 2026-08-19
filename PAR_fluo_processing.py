8f381c0631b134143e1ca5720916ce7a2ed1a653
COLESS <sashacoles@live.ca>
2026-08-19 16:26:08 -0500

    Initial commit

---
+++ PAR_fluo_processing.py
@@ -0,0 +1,9 @@
+"""
+author: Sasha Coles
+date: August 19th, 2026
+about: This script is for processing Concerto RBR sensor data (specifically fluorescence and PAR)
+This script is designed to process one profile at a time, and follows the processing
+guidelines outlined in Halverson et al. (2017).
+
+This code is based on the RBR CTD processing script found here: https://github.com/IOS-OSD-DPG/RBR-CTD-Processing/tree/main
+"""
